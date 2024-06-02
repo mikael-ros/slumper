e0127086f2fc3849abf60e9165d8a9b658f23291
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-06-02 20:54:58 +0200

    Added some search engine optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import solidJs from "@astrojs/solid-js";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  site: 'https://www.slumper.me',
+  integrations: [solidJs(), sitemap()]
 });
