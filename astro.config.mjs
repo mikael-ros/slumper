8abf20785b7f6a6d3b3f3d15ef8e4ba4e1ef1893
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-06-02 21:49:30 +0200

    Removed books, replaced with example book

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import sitemap from "@astrojs/sitemap";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.slumper.me',
-  integrations: [solidJs(), sitemap()]
+  integrations: [solidJs(), sitemap()],
+  output: "server",
+  adapter: netlify()
 });
