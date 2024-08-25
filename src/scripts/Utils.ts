872d95f273bccbd308647c2e22e629db1a122365
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-08-25 23:15:59 +0200

    Removed redundant warn function

--- src/scripts/Utils.ts
+++ src/scripts/Utils.ts
@@ -1,13 +1,4 @@
 /**
- * Displays to the user whether their input is valid
- * @param valid Whether the input is valid
- * @param event The event, such that we can change the color of the target
- */
-export function warn(valid: boolean, event : Event & {currentTarget : HTMLInputElement}){
-    event.currentTarget.style.color = valid ? "var(--text-color-negative)" : "red";
-}
-
-/**
  * Parses inputs
  * @param input The raw input string
  * @returns Either a string or an integer
