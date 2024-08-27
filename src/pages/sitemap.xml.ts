2171101580b2600f4882391ea432e0abdfb22703
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-08-27 23:16:52 +0200

    Further sitemap attempts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,6 +1,6 @@
 export async function GET() {
-    let result = `
-    <?xml version="1.0" encoding="UTF-8"?>
+    let result = 
+    `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         <url>
             <loc>https://www.slumper.me</loc>
