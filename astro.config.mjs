f9f406b56dd53f50d18d7e4db8fe65a778b2f5cb
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-08-27 23:02:21 +0200

    Removed sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://slumper.me',
-  integrations: [solidJs(), sitemap()],
+  integrations: [solidJs()],
   output: "server",
   adapter: netlify()
 });
