c3feeb3b322c056e9214c1364609f14b42d0ebc1
Mikael <112403418+mikael-ros@users.noreply.github.com>
2024-08-27 23:15:40 +0200

    Further sitemap attempts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,5 +1,16 @@
 export async function GET() {
-    let result = '';
+    let result = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://www.slumper.me</loc>
+            <priority>1.0</priority>
+        </url>
+        <url>
+            <loc>https://www.slumper.me/add</loc>
+            <priority>0.8</priority>
+        </url>
+    </urlset>`;
    
     return new Response(result, {
       headers: {
